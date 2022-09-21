b5027a5834d673afd4aa1bb617c80b2d917ce2d8
sayem52 <sayem15-12949@diu.edu.bd>
2022-09-22 01:07:39 +0600

    3rd

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ const start = () => {
 
 // START Countdown
 
-startBtn.addEventListener("click", start());
+startBtn.addEventListener("click", start);
 
 // If history exists, show it
 displayHistory();
