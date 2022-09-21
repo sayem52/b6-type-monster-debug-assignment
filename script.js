d4587d2159912f4e12abdb5207be28e4f7553795
sayem52 <sayem15-12949@diu.edu.bd>
2022-09-22 01:36:27 +0600

    4th

--- script.js
+++ script.js
@@ -140,8 +140,8 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeSpent = parseInt((currentTime - startTime) / 1000);
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0 } seconds`;
 }, 1000);
